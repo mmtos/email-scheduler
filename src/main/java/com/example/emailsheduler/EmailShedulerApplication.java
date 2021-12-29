a90076fa0244162b5a5a2eb554d74e39adddbe35
mmtos <js223kr92@naver.com>
2021-12-30 00:00:39 +0900

    start

---
+++ src/main/java/com/example/emailsheduler/EmailShedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailsheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailShedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailShedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emailsheduler/EmailShedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailsheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailShedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
